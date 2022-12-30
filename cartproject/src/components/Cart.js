4c9ee857a51c8f6f9f27705039fe4615c2641a16
Rudra Rao <rudra@yodaplus.com>
2022-12-30 13:07:15 +0530

    Some more changes

--- cartproject/src/components/Cart.js
+++ cartproject/src/components/Cart.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { useGlobalContext } from "../context/Context";
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
+import LoadingAnimation from "./Loading";
+import { Box } from "@mui/material";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const { loading } = useGlobalContext();
+  return loading ? (
+    <LoadingAnimation />
+  ) : (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Navbar />
+      <CartContainer />
+    </Box>
+  );
 };
 
 export default Cart;
